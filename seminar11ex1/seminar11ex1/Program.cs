3d665d8d0819e2cd8c723034a447480929579c70
Alexandraiovan17 <116071293+Alexandraiovan17@users.noreply.github.com>
2024-06-01 20:22:07 +0300

    Add files via upload

---
+++ seminar11ex1/seminar11ex1/Program.cs
@@ -0,0 +1,14 @@
+namespace seminar11ex1
+{
+    internal static class Program
+    {
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+}
